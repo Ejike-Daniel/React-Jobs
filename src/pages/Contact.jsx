071a2006039b9c77848a66989fe0748ea787d7a4
Elrey009 <daniloejike@gmail.com>
2025-02-28 15:08:23 +0100

    modified files

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import ContactGif from "../assets/images/Contact-us.gif";
+import { toast } from "react-toastify";
 
 const ContactPage = () => {
   const [formData, setFormData] = useState({
@@ -13,6 +15,7 @@ const ContactPage = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+         toast.success("Message sent successfully!");
 
     try {
       await fetch("http://localhost:3000/contacts", {
@@ -22,7 +25,6 @@ const ContactPage = () => {
         },
         body: JSON.stringify(formData),
       });
-      alert("Message sent successfully!");
       setFormData({ name: "", email: "", message: "" });
     } catch (error) {
       console.error("Error submitting form", error);
@@ -30,51 +32,57 @@ const ContactPage = () => {
   };
 
   return (
-    <section className="container mx-auto p-6">
-      <h2 className="text-3xl font-bold mb-4">Contact Us</h2>
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white p-6 shadow-md rounded-md"
-      >
-        <div className="mb-4">
-          <label className="block text-gray-700">Name</label>
-          <input
-            type="text"
-            name="name"
-            className="w-full border p-2 rounded"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700">Email</label>
-          <input
-            type="email"
-            name="email"
-            className="w-full border p-2 rounded"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700">Message</label>
-          <textarea
-            name="message"
-            className="w-full border p-2 rounded"
-            value={formData.message}
-            onChange={handleChange}
-            required
-          ></textarea>
-        </div>
-        <button
-          type="submit"
-          className="bg-indigo-500 text-white px-4 py-2 rounded"
+    <section className="container mx-auto p-6 ">
+      <h2 className="text-3xl font-bold mb-4 py-2 text-center">Contact Us</h2>
+      <div className="flex flex-col lg:flex-row justify-between items-center">
+        {" "}
+        <form
+          onSubmit={handleSubmit}
+          className="bg-white py-6  px-8 h-[30%] shadow-md rounded-md w-[50%]"
         >
-          Send Message
-        </button>
-      </form>
+          <fieldset className="mb-4 pb-2">
+            <label className="block text-gray-700">Name</label>
+            <input
+              type="text"
+              name="name"
+              className="w-full border p-2 rounded"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </fieldset>
+          <fieldset className="mb-4 pb-2">
+            <label className="block text-gray-700">Email</label>
+            <input
+              type="email"
+              name="email"
+              className="w-full border p-2 rounded"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </fieldset>
+          <fieldset className="mb-4 pb-2">
+            <label className="block text-gray-700">Message</label>
+            <textarea
+              name="message"
+              className="w-full border p-2 rounded"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            ></textarea>
+          </fieldset>
+          <button
+            type="submit"
+            className="bg-indigo-500 text-white px-4 pb-2 py-2 rounded"
+          >
+            Send Message
+          </button>
+        </form>
+        <div className="w-[50%]">
+          <img src={ContactGif} alt="Contact gif" />
+        </div>
+      </div>
     </section>
   );
 };
